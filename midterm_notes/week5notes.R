906d18311a1115f166587554774a909c2104d493
Peter <pkhrissanov@gmail.com>
2026-02-24 21:09:45 -0800

    week 5 notes complete

--- midterm_notes/week5notes.R
+++ midterm_notes/week5notes.R
@@ -35,23 +35,79 @@ query <- sqlInterpolate(db, "SELECT SUM(Quantity) FROM sales
                         month = paste0(months[i], "%")
 )
 
+
+#how to do sql interprolate well 
+library(DBI)
+con <- dbConnect(RSQLite::SQLite(), ":memory:")
+sql <- "SELECT * FROM users WHERE name = ?name AND age > ?age"
+query <- sqlInterpolate(con, sql, name = "John", age = 25)
+# Output: SELECT * FROM users WHERE name = 'John' AND age > 25
+
+
+
+
+
+
 request <- dbSendQuery(db, query)
 
-a <- 2      # intercept
-b  <- 0.5    # slope
 
+
+a <- 2  
+b  <- 0.5   
 x <- c(0, 1, 2, 3, 4, 5)
-
 y <- c(2.1, 2.4, 3.2, 3.9, 4.1, 4.8)
-
 
 #sum of least squares. 
 SSE <- function(alpha, beta, x, y){
   sum((y - (alpha + beta * x))^2)
 }
 
-SSE(a, b, x, y)
 
-SSE(a, b, x, y)
+SSE <- function(alpha, beta, x, y){
+  result <- 0
+  
+  for(i in 1:length(x)){
+    result <- result + (y[i] - (alpha + beta * x[i]))^2
+  }
+  
+  return(result)
+}
+
+
+
+# linear model function  --------------------------------------------------
+
+df <- data.frame(
+  x = c(1,2,3,4,5),
+  y = c(2,4,5,4,5)
+)
+
+
+lin_model <- lm(y ~ x, data = df)
+#how to extract alpha and beta
+
+a <- as.numeric(coef(lin_model)[1]) #intercept, or alpha
+b <- as.numeric(coef(lin_model)[2]) #slope, or beta
+c <- (a+b)
+c
+
+
+
+#or can do : 
+xs <- c(1, 2, 3)
+ys <- c(73, 42, 55)
+model <- lm(y ~ x, data = data.frame(x = xs, y = ys))
+alpha <- summary(model)$coefficients[1, 1]
+beta <- summary(model)$coefficients[2, 1]
+alpha
+
+
+
+
+
+
+
+
+
 
 ...
